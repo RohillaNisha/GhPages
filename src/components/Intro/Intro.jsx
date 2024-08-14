66684a57169967a1aecf3168402ddad7b1ba0659
Nisha <nisha.bioinfo@gmail.com>
2024-08-14 17:28:15 +0200

    changed intro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,4 +1,5 @@
 import profile from '../../assets/profile.png'
+import hi from '../../assets/hi.png'
 import hire from '../../assets/hireme.png'
 import {Link} from 'react-scroll'
 import './intro.css'
@@ -14,11 +15,12 @@ const Intro = () => {
   return (
     <section id="intro">
       <div className="introContent">
-        <span className="hello">Hello,</span>
-        <span className="introText">I am  <span className="introName">Nisha</span> <br />Fullstack Web Developer </span>
-        <p className="introPara">Crafting Code, Creating Impact: Your Fullstack Web Developer for <br/> meaningful applications </p>
-        <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-70} duration={500} onClick={handleHireMeClick}> <button className="hireMeBtn"> <img className="btnImg" src={hire} alt="hire me" /> Hire Me</button> </Link>
-
+        <span className="hello">Hello !   <img src={hi} alt="hello hand" className="handIcon"/> My name is </span>
+        <span className="introText"><span className="introName"> NISHA </span> ROHILLA </span>
+        <span className="iAmFullStack"> I am a Fullstack Developer </span>
+        {/* <span className="introText">I am  <span className="introName">Nisha</span> <br />Fullstack Web Developer </span> */}
+        {/* <p className="introPara">Crafting Code, Creating Impact: Your Fullstack Web Developer for <br/> meaningful applications </p> */}
+        {/* <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-70} duration={500} onClick={handleHireMeClick}> <button className="hireMeBtn"> <img className="btnImg" src={hire} alt="hire me" /> Hire Me</button> </Link> */}
       </div>
       <img src={profile} alt="Profile" className="bg" />
     </section>
