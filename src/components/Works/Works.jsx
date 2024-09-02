07744d126dcab953f5c96c82b4e565f3a4522ba7
Nisha <nisha.bioinfo@gmail.com>
2024-09-02 11:18:37 +0200

    added links to projects

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './works.css'
 import RPS from '../../assets/RPS.png'
 import CV from '../../assets/CV.png'
+import { ExternalLink } from 'react-external-link'
 
 function Works() {
   return (
     <section id="works">
-    <h2 className="worksTitle">My Portfolio</h2>
+    <h2 className="worksTitle">My Projects</h2>
     <span className="worksDesc">    Self-motivated, pro-active and enthusiastic fullstack
                                     JavaScript developer. After serving in a public sector bank
                                     for quite a long time, I wish to change my career and dare
@@ -14,12 +15,12 @@ function Works() {
                                     and grow as a JavaScript developer and get more
                                     knowledge within frontend and backend development. </span>
     <div className="worksImgs">
-        <img src={RPS} alt="rock paper scissors image" className="worksImg" />
+        <ExternalLink href="https://rohillanisha.github.io/Rock-Paper-Scissors/"> <img src={RPS} alt="rock paper scissors image" className="worksImg" /> </ExternalLink>
+        <ExternalLink href="https://rohillanisha.github.io/CV-assignment-Teknikh-gskolan/"> <img src={CV} alt="CV image" className="worksImg" /> </ExternalLink>
+        {/* <img src={RPS} alt="rock paper scissors image" className="worksImg" />
         <img src={CV} alt="CV image" className="worksImg" />
         <img src={RPS} alt="rock paper scissors image" className="worksImg" />
-        <img src={CV} alt="CV image" className="worksImg" />
-        <img src={RPS} alt="rock paper scissors image" className="worksImg" />
-        <img src={CV} alt="CV image" className="worksImg" />
+        <img src={CV} alt="CV image" className="worksImg" /> */}
     </div>
     <button className="worksBtn"> See more</button>
 
